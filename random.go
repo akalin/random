09d092a431e3bf505c636d380e83d00bc4e065b5
Frederick Akalin <akalin@gmail.com>
2019-08-22 00:43:16 -0400

    Rework implementation
    Also make testing more robust.

--- random.go
+++ random.go
@@ -8,13 +8,21 @@ func UniformUint32(src Source, n uint32) uint32 {
 	v := src.Uint32()
 	prod := uint64(v) * uint64(n)
 	low := uint32(prod)
-	if low < uint32(n) {
-		thresh := uint32(-n) % uint32(n)
-		for low < thresh {
-			v = src.Uint32()
-			prod = uint64(v) * uint64(n)
-			low = uint32(prod)
+	if low >= n {
+		return uint32(prod >> 32)
+	}
+
+	thresh := uint32(-n) % uint32(n)
+	if low >= thresh {
+		return uint32(prod >> 32)
+	}
+
+	for {
+		v = src.Uint32()
+		prod = uint64(v) * uint64(n)
+		low = uint32(prod)
+		if low >= thresh {
+			return uint32(prod >> 32)
 		}
 	}
-	return uint32(prod >> 32)
 }
--- random_test.go
+++ random_test.go
@@ -10,30 +10,32 @@ func uniformUint32_simple(src Source, n uint32) uint32 {
 		v := src.Uint32()
 		prod := uint64(v) * uint64(n)
 		low := uint32(prod)
-		if low > thresh {
+		if low >= thresh {
 			return uint32(prod >> 32)
 		}
 	}
 }
 
-type sequentialSource struct {
+type descendingSource struct {
 	i uint32
 }
 
-func (src *sequentialSource) Uint32() uint32 {
-	v := src.i
-	src.i++
-	return v
+func (src *descendingSource) Uint32() uint32 {
+	src.i--
+	return src.i
 }
 
 func TestUintformUint32MatchesSimple(t *testing.T) {
-	var src1, src2 sequentialSource
-	var n uint32 = 129
-	for i := 0; i < 10000; i++ {
+	var src1, src2 descendingSource
+	var n uint32 = 0xffffffff
+	for i := 0; i < 1000; i++ {
 		expected := uniformUint32_simple(&src1, n)
 		actual := UniformUint32(&src2, n)
 		if expected != actual {
 			t.Fatalf("(i = %d) expected %d, got %d", i, expected, actual)
+		}
+		if src1.i != src2.i {
+			t.Fatalf("(i = %d) expected %d, got %d", i, src1.i, src2.i)
 		}
 	}
 }
