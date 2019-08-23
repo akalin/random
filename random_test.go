243ae9c04882e51ce8f4c0ba3f1e63da386440d5
Frederick Akalin <akalin@gmail.com>
2019-08-23 14:47:01 -0400

    Add comprehensive test

--- random_test.go
+++ random_test.go
@@ -1,8 +1,47 @@
 package random
 
 import (
+	"fmt"
 	"testing"
 )
+
+type singleSource struct {
+	i      uint32
+	called bool
+}
+
+func (src *singleSource) Uint32() uint32 {
+	if src.called {
+		panic("already called")
+	}
+	src.called = true
+	return src.i
+}
+
+func TestUniformUint32IsUniform(t *testing.T) {
+	t.Skip()
+
+	var n uint32 = 5
+	buckets := make([]int, n)
+	thresh := uint32(-n) % uint32(n)
+	for i := uint64(0); i <= 0xffffffff; i++ {
+		if i&0x0fffffff == 0 {
+			fmt.Printf("i is %x\n", i)
+		}
+		if uint32(i*uint64(n)) < thresh {
+			continue
+		}
+		src := singleSource{i: uint32(i)}
+		actual := UniformUint32(&src, n)
+		buckets[actual]++
+	}
+	expectedCount := buckets[0]
+	for i := uint32(1); i < n; i++ {
+		if buckets[i] != expectedCount {
+			t.Errorf("(i = %d) expected %d, got %d", i, expectedCount, buckets[i])
+		}
+	}
+}
 
 func uniformUint32Simple(src Source, n uint32) uint32 {
 	thresh := uint32(-n) % uint32(n)
@@ -25,7 +64,7 @@ func (src *descendingSource) Uint32() uint32 {
 	return src.i
 }
 
-func TestUintformUint32MatchesSimple(t *testing.T) {
+func TestUniformUint32MatchesSimple(t *testing.T) {
 	var src1, src2 descendingSource
 	var n uint32 = 0xffffffff
 	for i := 0; i < 1000; i++ {
