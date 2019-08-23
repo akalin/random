53f8963c81ed5a7b93c7a5e3b37707e3f82d6aaa
Frederick Akalin <akalin@gmail.com>
2019-08-23 15:38:26 -0400

    Better range test

--- random_test.go
+++ random_test.go
@@ -6,28 +6,36 @@ import (
 )
 
 type singleSource struct {
-	i      uint32
-	called bool
+	i         uint32
+	callCount uint32
 }
 
 func (src *singleSource) Uint32() uint32 {
-	if src.called {
-		panic("already called")
+	if src.callCount == 0 {
+		src.callCount++
+		return src.i
 	}
-	src.called = true
-	return src.i
+
+	if src.callCount == 1 {
+		src.callCount++
+		return 0xffffffff
+	}
+
+	panic("called when callCount > 1")
 }
 
-func computeRange(i, n uint32) (uint32, uint32) {
-	thresh := uint32(-n) % uint32(n)
-	firstProd := uint64(i)<<32 + uint64(thresh)
-	lastProd := uint64(i+1)<<32 - 1
-	firstV := uint32((firstProd + uint64(n) - 1) / uint64(n))
-	lastV := uint32(lastProd / uint64(n))
-	return firstV, lastV
+func computeRange(i, n uint32) (uint32, uint32, uint32) {
+	thresh := -n % n
+	firstProd := uint64(i) << 32
+	firstValidProd := uint64(i)<<32 + uint64(thresh)
+	lastValidProd := uint64(i+1)<<32 - 1
+	firstV := uint32((firstProd + uint64(n-1)) / uint64(n))
+	firstValidV := uint32((firstValidProd + uint64(n-1)) / uint64(n))
+	lastValidV := uint32(lastValidProd / uint64(n))
+	return firstV, firstValidV, lastValidV
 }
 
-func expectUniformUint32(t *testing.T, n, v, expected uint32) {
+func expectUniformUint32Returns(t *testing.T, n, v, expected uint32) {
 	t.Helper()
 	src := singleSource{i: v}
 	actual := UniformUint32(&src, n)
@@ -36,12 +44,47 @@ func expectUniformUint32(t *testing.T, n, v, expected uint32) {
 	}
 }
 
+func expectUniformUint32Rejects(t *testing.T, n, v uint32) {
+	t.Helper()
+	src := singleSource{i: v}
+	actual := UniformUint32(&src, n)
+	if src.callCount != 2 {
+		t.Errorf("(v=%d) expected %d, got %d (actual=%d)", v, 2, src.callCount, actual)
+	}
+}
+
 func TestUniformUint32Range(t *testing.T) {
 	var n uint32 = 5
+	var prevLastValidV uint32
 	for i := uint32(0); i < n; i++ {
-		firstV, lastV := computeRange(i, n)
-		expectUniformUint32(t, n, firstV, i)
-		expectUniformUint32(t, n, lastV, i)
+		firstV, firstValidV, lastValidV := computeRange(i, n)
+		if i == 0 {
+			if firstV != 0 {
+				t.Errorf("(i=0) expected firstV=0, got %d", firstV)
+			}
+		} else if prevLastValidV+1 != firstV {
+			t.Errorf("(i=%d) expected firstV=%d to follow prevLastValidV=%d", i, firstV, prevLastValidV)
+		}
+
+		if i == n-1 {
+			if lastValidV != 0xffffffff {
+				t.Errorf("(i=%d) expected lastValidV=0xffffffff, got %d", i, lastValidV)
+			}
+		}
+
+		for i := firstV; i < firstValidV; i++ {
+			expectUniformUint32Rejects(t, n, i)
+		}
+		expectUniformUint32Returns(t, n, firstValidV, i)
+		delta := (lastValidV - firstValidV) / 1000
+		if delta == 0 {
+			delta = 1
+		}
+		for j := uint64(firstValidV) + 1; j < uint64(lastValidV); j += uint64(delta) {
+			expectUniformUint32Returns(t, n, uint32(j), i)
+		}
+		expectUniformUint32Returns(t, n, lastValidV, i)
+		prevLastValidV = lastValidV
 	}
 }
 
@@ -50,7 +93,7 @@ func TestUniformUint32IsUniform(t *testing.T) {
 
 	var n uint32 = 5
 	buckets := make([]int, n)
-	thresh := uint32(-n) % uint32(n)
+	thresh := -n % n
 	for i := uint64(0); i <= 0xffffffff; i++ {
 		if i&0x0fffffff == 0 {
 			fmt.Printf("i is %x\n", i)
