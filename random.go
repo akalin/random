587b9edef6476616695a333207ff7862f23cf988
Frederick Akalin <akalin@gmail.com>
2019-08-23 22:34:42 -0400

    Talk about small and big groups

--- random.go
+++ random.go
@@ -53,14 +53,16 @@ are:
 
 where we're grouping the table by values of high, which takes values from 0 to n-1.
 
-As mentioned above, because n doesn't evenly divide 2³, each group has either floor(2³/n) = 2 or ceil(2³/n) = 3 values,
-which means that if we simply returned high, we wouldn't get a uniform distribution from 0 to n-1.
+As mentioned above, because n doesn't evenly divide 2³, each group has either floor(2³/n) = 2 (a small group) or
+ceil(2³/n) = 3 values (a big group), which means that if we simply returned high,
+we wouldn't get a uniform distribution from 0 to n-1.
 
 We want to use the value of low to decide which values of v to reject. In this case, if we reject all values of v
 where low < 2, then we'd throw out v=0 and v=3, so each group would have exactly 2 values.
 
 Now the question is: how we decide what threshold to set for low? The first fact we use is that the group with v=0
-contains ceil(2³/n) entries. This is because that group contains exactly the values of v such that 0 ≤ v*n < 2³.
+is a big group.contains. This is because that group contains exactly the values of v such that 0 ≤ v*n < 2³,
+and there are exactly ceil(2³/n) such values.
 
 The second fact we use is that the leftmost entry in each group has 0 ≤ low < n. This is because if an entry in a group
 has low > n, then moving left decreases the value of prod and low by n but leaves high the same.
@@ -79,7 +81,7 @@ If we do it again, we get:
  2 5 (8)
 
 where the last value is ≥ 2³, so it actually belongs to the following group. So the smallest value of low
-where we get a "small" group is 2³ minus 6, the rightmost value of low of the first group.
+where we get a small group is 2³ minus 6, the rightmost value of low of the first group.
 
 Now we can derive a general formula for the threshold value. The rightmost value of low of the first group
 is the largest multiple of n < 2³, which is
@@ -90,8 +92,8 @@ Therefore, the threshold value is
 
   2³ - (2³ - (2³ % n)) = 2³ % n.
 
-That is, if we filter out values of v where low < 2³ % n, then we remove a single entry from each "big" group,
-turning into a "small" group. Then all groups would have the same size, and we'd have a uniform distribution.
+That is, if we filter out values of v where low < 2³ % n, then we remove a single entry from each big group,
+turning into a small group. Then all groups would have the same size, and we'd have a uniform distribution.
 
 Now the same logic holds if we work with 32-bit integers; we just replace 2³ with 2³² everywhere.
 Now recall that
