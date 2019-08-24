5ad6af427c971077936ef4469a20feb576d898fe
Frederick Akalin <akalin@gmail.com>
2019-08-24 04:13:12 -0400

    Remove last test

--- random_test.go
+++ random_test.go
@@ -169,40 +169,4 @@ func TestUniformUint32(t *testing.T) {
 	}
 }
 
-func uniformUint32Simple(src Source, n uint32) uint32 {
-	thresh := uint32(-n) % uint32(n)
-	for {
-		v := src.Uint32()
-		prod := uint64(v) * uint64(n)
-		low := uint32(prod)
-		if low >= thresh {
-			return uint32(prod >> 32)
-		}
-	}
-}
-
-type descendingSource struct {
-	i uint32
-}
-
-func (src *descendingSource) Uint32() uint32 {
-	src.i--
-	return src.i
-}
-
-func TestUniformUint32MatchesSimple(t *testing.T) {
-	var src1, src2 descendingSource
-	var n uint32 = 0xffffffff
-	for i := 0; i < 1000; i++ {
-		expected := uniformUint32Simple(&src1, n)
-		actual := UniformUint32(&src2, n)
-		if expected != actual {
-			t.Fatalf("(i = %d) expected %d, got %d", i, expected, actual)
-		}
-		if src1.i != src2.i {
-			t.Fatalf("(i = %d) expected %d, got %d", i, src1.i, src2.i)
-		}
-	}
-}
-
 // TODO: Benchmarks.
